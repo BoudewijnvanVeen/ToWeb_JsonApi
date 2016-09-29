04b1df726852915e24836372a83deb223b92ad3e
Boudewijnvan Veen <bsvanveen@gmail.com>
2016-09-29 16:34:00 +0200

    added cors

--- ToWeb.JsonApi/App_Start/WebApiConfig.cs
+++ ToWeb.JsonApi/App_Start/WebApiConfig.cs
@@ -7,6 +7,7 @@ namespace ToWeb.JsonApi
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.EnableCors();
 
             // Web API routes
             config.MapHttpAttributeRoutes();
