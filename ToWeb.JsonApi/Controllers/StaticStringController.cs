fc7be47a5b53ca9373c33ec4a0fb42eaa5604894
Boudewijnvan Veen <bsvanveen@gmail.com>
2016-12-20 16:23:40 +0100

    removed comments

--- ToWeb.JsonApi/Controllers/StaticStringController.cs
+++ ToWeb.JsonApi/Controllers/StaticStringController.cs
@@ -1,25 +1,25 @@
 ﻿
-//using System.Web.Http;
+using System.Web.Http;
 
-//namespace ToWeb.JsonApi.Controllers
-//{
-//    public class StaticStringController : ApiController
-//    {
-//        public string Get()
-//        {
-//            return _store;
-//        }
-        
-//        public void Put(string json)
-//        {
-//            _store = json;
-//        }
-        
-//        public void Delete()
-//        {
-//            _store = string.Empty;
-//        }
+namespace ToWeb.JsonApi.Controllers
+{
+    public class StaticStringController : ApiController
+    {
+        public string Get()
+        {
+            return _store;
+        }
 
-//        private static string _store = string.Empty;
-//    }
-//}
+        public void Put(string json)
+        {
+            _store = json;
+        }
+
+        public void Delete()
+        {
+            _store = string.Empty;
+        }
+
+        private static string _store = string.Empty;
+    }
+}
--- ToWeb.JsonApi/Controllers/StaticStringDictionaryController.cs
+++ ToWeb.JsonApi/Controllers/StaticStringDictionaryController.cs
@@ -1,48 +1,48 @@
 ﻿
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Web.Http;
-//using ToWeb.JsonApi.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using ToWeb.JsonApi.Model;
 
-//namespace ToWeb.JsonApi.Controllers
-//{
-//    public class StaticStringDictionaryController : ApiController
-//    {
-//        private static Dictionary<Guid, string> _store = new Dictionary<Guid, string>();
+namespace ToWeb.JsonApi.Controllers
+{
+    public class StaticStringDictionaryController : ApiController
+    {
+        private static Dictionary<Guid, string> _store = new Dictionary<Guid, string>();
 
-//        public string Get(Guid id)
-//        {
-//            return _store.FirstOrDefault(c => c.Key.Equals(id)).Value;
-//        }
+        public string Get(Guid id)
+        {
+            return _store.FirstOrDefault(c => c.Key.Equals(id)).Value;
+        }
 
-//        [HttpGet]
-//        public string GetAll()
-//        {
-//            return $"[{string.Join(",", _store.Values)}]";
-//        }
+        [HttpGet]
+        public string GetAll()
+        {
+            return $"[{string.Join(",", _store.Values)}]";
+        }
 
-//        [HttpPost]
-//        public string Post(JsonContainer container)
-//        {
-//            if (!_store.ContainsKey(container.Key))
-//            {
-//                _store.Add(container.Key, container.Record);
-//            }
-//            else
-//            {
-//                _store[container.Key] = container.Record;
-//            }
+        [HttpPost]
+        public string Post(JsonContainer container)
+        {
+            if (!_store.ContainsKey(container.Key))
+            {
+                _store.Add(container.Key, container.Record);
+            }
+            else
+            {
+                _store[container.Key] = container.Record;
+            }
 
-//            return container.Record;
-//        }
-        
-//        [HttpDelete]
-//        public string Delete(Guid key)
-//        {
-//            _store.Remove(key);
+            return container.Record;
+        }
 
-//            return key.ToString();
-//        }
-//    }
-//}
+        [HttpDelete]
+        public string Delete(Guid key)
+        {
+            _store.Remove(key);
+
+            return key.ToString();
+        }
+    }
+}
